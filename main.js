d7a2511ef80f311a6eeba618c1a3ab2ac188ab00
Hola32ww3 <108763129+Hola32ww3@users.noreply.github.com>
2022-12-29 15:53:58 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function starClassification()
+{
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/pVNYCTyWF/model.json', modelReady);
+}
+function modelReady(){
+    classifier.classify( gotResults);
+}
