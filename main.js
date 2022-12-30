f7bdab8356daa72e977fcf69a1ef3654ddc0d2a7
Hola32ww3 <108763129+Hola32ww3@users.noreply.github.com>
2022-12-30 15:54:38 -0600

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,32 @@ function starClassification()
 function modelReady(){
     classifier.classify( gotResults);
 }
+function gotResults(error, results) {
+    if(error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_label").innerHTML = 'Escucho - '+ results[0].label;
+        document.getElementById("result_confidence").innerHTML= 'Precisión - '+ (results[0].confidence*100).toFixed(2)+"%";
+        img = document.getElementById('alien1');
+        img1 = document.getElementById('alien2');
+        img2 = document.getElementById('alien3');
+        if (results[0].label == "aplausos") {
+            img.classList.remove("girar");
+            void img.offsetWidth;
+            img.classList.add("girar");
+            img1.src = 'alien2.png'
+            img2.src = 'alien3.png'
+        } else if (results[0].label == "agua") {
+            img.src = 'alien1.png'
+            img1.classList.remove("girar");
+            void img1.offsetWidth;
+            img1.classList.add("girar");
+            img2.src = 'alien3.png'
+        } else{
+            img.src = 'alien1.png'
+            img1.src = 'alien2.png'
+            img2.src = 'alien3.gif'
+        }
+    }
+}
